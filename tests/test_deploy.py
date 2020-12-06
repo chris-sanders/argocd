50ec9056a607e2afd6added8a217a0fb68cf8c2e
Chris Sanders <sanders.chris@gmail.com>
2020-12-06 16:15:37 -0600

    Try again with minio fixed

--- tests/test_deploy.py
+++ tests/test_deploy.py
@@ -37,18 +37,21 @@ class TestDeploy:
         self.kubectl.apply("./acme-dns/apps/acme-dns-lab.yaml")
         self.argo.sync_app("acmedns", "./acme-dns/")
         self.kubernetes.wait_containers_ready("acmedns")
+        self._check_clean("acmedns")
 
     def test_bitwardenrs_install(self):
         """Test applying the app"""
         self.kubectl.apply("./bitwardenrs/apps/bitwardenrs-lab.yaml")
         self.argo.sync_app("bitwarden", "./bitwardenrs/")
         self.kubernetes.wait_containers_ready("bitwarden")
+        self._check_clean("bitwarden")
 
     def test_cert_manager_install(self):
         """Test applying the app"""
         self.kubectl.apply("./cert-manager/apps/cert-manager-lab.yaml")
         self.argo.sync_app("cert-manager", "./cert-manager/")
         self.kubernetes.wait_containers_ready("cert-manager")
+        self._check_clean("cert-manager")
 
     @pytest.mark.slow
     def test_gitlab_install(self):
@@ -60,6 +63,7 @@ class TestDeploy:
         self.kubectl.apply("./gitlab/apps/gitlab-lab.yaml")
         # self.argo.sync_app("gitlab", "./gitlab/")
         self.kubernetes.wait_containers_ready("gitlab", timeout=900)
+        self._check_clean("gitlab")
 
     def test_goldilocks_install(self):
         """Test applying the app"""
@@ -68,30 +72,35 @@ class TestDeploy:
         self.kubernetes.wait_containers_ready(
             "llama", "app.kubernetes.io/name=goldilocks"
         )
+        self._check_clean("goldilocks")
 
     def test_kps_install(self):
         """Test applying the app"""
         self.kubectl.apply("./llama/kube-prometheus-stack/apps/kps-lab.yaml", sync=True)
         # self.argo.sync_app("kube-prometheus-stack", "./llama/kube-prometheus-stack/")
         self.kubernetes.wait_containers_ready("llama", timeout=120)
+        self._check_clean("kube-prometheus-stack")
 
     def test_minecraft_install(self):
         """Test applying the app"""
         self.kubectl.apply("./minecraft/apps/minecraft-lab.yaml")
         self.argo.sync_app("minecraft", "./minecraft/")
         self.kubernetes.wait_containers_ready("minecraft")
+        self._check_clean("minecraft")
 
     def test_pihole_install(self):
         """Test applying the app"""
         self.kubectl.apply("./pihole/apps/pihole-lab.yaml")
         self.argo.sync_app("pihole", "./pihole/")
         self.kubernetes.wait_containers_ready("pihole")
+        self._check_clean("pihole")
 
     def test_sealed_secrets_install(self):
         """Test applying the app"""
         self.kubectl.apply("./sealed-secrets/apps/sealed-secrets-lab.yaml")
         self.argo.sync_app("sealed-secrets-controller", "./sealed-secrets/")
         self.kubernetes.wait_containers_ready("sealed-secrets")
+        self._check_clean("sealed-secrets-controller")
 
     def test_duplicati_install(self):
         """Test applying the app"""
@@ -100,37 +109,42 @@ class TestDeploy:
         self.kubernetes.wait_containers_ready(
             "services", "app.kubernetes.io/name=duplicati"
         )
+        self._check_clean("duplicati")
 
-    @pytest.mark.gh_skip
     def test_minio_install(self):
         """Test applying the app"""
         self.kubectl.apply("./services/minio/apps/minio-lab.yaml")
         self.argo.sync_app("minio", "./services/minio/")
         self.kubernetes.wait_containers_ready("services", "app=minio")
+        self._check_clean("minio")
 
     def test_samba_install(self):
         """Test applying the app"""
         self.kubectl.apply("./services/samba/apps/samba-lab.yaml")
         self.argo.sync_app("samba", "./services/samba/")
         self.kubernetes.wait_containers_ready("services", "app=samba")
+        self._check_clean("samba")
 
     def test_sftp_install(self):
         """Test applying the app"""
         self.kubectl.apply("./services/sftp/apps/sftp-lab.yaml")
         self.argo.sync_app("sftp", "./services/sftp/")
         self.kubernetes.wait_containers_ready("services", "app=sftp")
+        self._check_clean("sftp")
 
     def test_unifi_install(self):
         """Test applying the app"""
         self.kubectl.apply("./unifi/apps/unifi-lab.yaml")
         self.argo.sync_app("unifi", "./unifi/")
         self.kubernetes.wait_containers_ready("unifi")
+        self._check_clean("unifi")
 
     def test_nzbget_install(self):
         """Test applying the app"""
         self.kubectl.apply("./usenet/nzbget/apps/nzbget-lab.yaml")
         self.argo.sync_app("nzbget", "./usenet/nzbget/")
         self.kubernetes.wait_containers_ready("usenet", "app.kubernetes.io/name=nzbget")
+        self._check_clean("nzbget")
 
     def test_nzbhydra_install(self):
         """Test applying the app"""
@@ -139,6 +153,7 @@ class TestDeploy:
         self.kubernetes.wait_containers_ready(
             "usenet", "app.kubernetes.io/name=nzbhydra2"
         )
+        self._check_clean("nzbhydra")
 
     def test_organizr_install(self):
         """Test applying the app"""
@@ -147,18 +162,21 @@ class TestDeploy:
         self.kubernetes.wait_containers_ready(
             "usenet", "app.kubernetes.io/name=organizr", timeout=180
         )
+        self._check_clean("organizr")
 
     def test_plex_install(self):
         """Test applying the app"""
         self.kubectl.apply("./usenet/plex/apps/plex-lab.yaml")
         self.argo.sync_app("plex", "./usenet/plex/")
         self.kubernetes.wait_containers_ready("usenet", "app.kubernetes.io/name=plex")
+        self._check_clean("plex")
 
     def test_radarr_install(self):
         """Test applying the app"""
         self.kubectl.apply("./usenet/radarr/apps/radarr-lab.yaml")
         self.argo.sync_app("radarr", "./usenet/radarr/")
         self.kubernetes.wait_containers_ready("usenet", "app.kubernetes.io/name=radarr")
+        self._check_clean("radarr")
 
     def test_sabnzbd_install(self):
         """Test applying the app"""
@@ -167,12 +185,14 @@ class TestDeploy:
         self.kubernetes.wait_containers_ready(
             "usenet", "app.kubernetes.io/name=sabnzbd"
         )
+        self._check_clean("sabnzbd")
 
     def test_sonarr_install(self):
         """Test applying the app"""
         self.kubectl.apply("./usenet/sonarr/apps/sonarr-lab.yaml")
         self.argo.sync_app("sonarr", "./usenet/sonarr/")
         self.kubernetes.wait_containers_ready("usenet", "app.kubernetes.io/name=sonarr")
+        self._check_clean("sonarr")
 
     # def test_velero_install(self):
     #     """Test applying the app"""
